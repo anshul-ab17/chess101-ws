86ded0d34ff045488ce00c14307321f0eab6d0ff
Anshul Bharat <anshul.ab17x@gmail.com>
2025-10-29 00:19:16 +0530

    gameManager

--- server/src/Game.ts
+++ server/src/Game.ts
@@ -1,21 +1,23 @@
 import { WebSocket } from "ws";
+import {Chess} from "chess.js"
 
 export class Game {
     public player1:WebSocket;
     public player2:WebSocket;
-    private board:string;
-    private moves:string[];
+    private board:Chess 
     private startTime:Date;
 
 
     constructor(player1:WebSocket, player2:WebSocket){
         this.player1=player1;
         this.player2=player2;
-        this.board="";
-        this.moves=[];
+        this.board=new Chess(); 
         this.startTime=new Date();
     }
-    makeMove(socket:WebSocket, move:string){
+    makeMove(socket:WebSocket, move:{
+        from:string;
+        to:string
+    }){
 
     }
 }
